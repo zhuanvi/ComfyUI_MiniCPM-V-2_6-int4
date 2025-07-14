ca4a617a3577666ca656a0afd406b9a493359cf6
zhuanvi <570868110@qq.com>
2025-07-14 19:17:30 +0800

    fix LoadVideo in New ComfyUI

--- __init__.py
+++ __init__.py
@@ -1,14 +1,11 @@
 from .nodes_legacy import MiniCPM_VQA
 from .nodes_polished import MiniCPM_VQA_Polished
 from .image_nodes import MultipleImagesInput
-from .util_nodes import LoadVideo,PreviewVideo
 from .display_text_nodes import DisplayText
 WEB_DIRECTORY = "./web"
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "LoadVideo": LoadVideo,
-    "PreviewVideo": PreviewVideo,
     "MultipleImagesInput": MultipleImagesInput,
     "MiniCPM_VQA": MiniCPM_VQA,
     "MiniCPM_VQA_Polished": MiniCPM_VQA_Polished,
@@ -17,8 +14,6 @@ NODE_CLASS_MAPPINGS = {
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LoadVideo": "Load Video",
-    "PreviewVideo": "Preview Video",
     "MultipleImagesInput": "Multiple Images Input",
     "MiniCPM_VQA": "MiniCPM VQA",
     "MiniCPM_VQA_Polished": "MiniCPM VQA Polished",
